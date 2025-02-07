eba577e4809fce9a7b5a259ece1266193a42cea7
acodered <alexkondratru@gmail.com>
2025-02-07 12:59:08 +0100

    Add a main backend file

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from app.routes.detection import router as detection_router
 
 app = FastAPI(title="YOLO Image Detection API")
-
+app.include_router(detection_router)
 
 @app.get("/")
 async def root():
